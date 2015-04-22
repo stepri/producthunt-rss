959c7bcbcc70baa6ab19704b5c47895aac37f442
Steven Prins <mail@stevenprins.com>
2015-04-22 13:52:52 +0200

    Fixed typos, add cache

--- app.js
+++ app.js
@@ -1,55 +1,70 @@
-var express = require('express');
 var OAuth = require('oauth');
 var RSS = require('rss');
 var _ = require('underscore');
+var cache = require('memory-cache');
+
+var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.redirect('/rss/post/all');
+  res.redirect('/rss/posts/all');
 });
 
-app.get('/rss/post/all', function (req, res) {
+app.get('/rss/posts/all', function (req, res) {
 
-  var oa = new OAuth.OAuth2(
-    process.env.API_KEY,
-    process.env.API_SECRET,
-    'https://api.producthunt.com/',
-    'oauth/authorize',
-    'oauth/token',
-    null
-  );
+  req.cached = cache.get('posts/all');
 
-  oa.get('https://api.producthunt.com/v1/posts/all', process.env.DEV_TOKEN, function(err, data) {
+  if (req.cached) {
 
-    if (err) {
+    res.send(req.cached);
 
-      res.send(err);
+  } else {
 
-    } else {
+    var oa = new OAuth.OAuth2(
+      process.env.API_KEY,
+      process.env.API_SECRET,
+      'https://api.producthunt.com/',
+      'oauth/authorize',
+      'oauth/token',
+      null
+    );
 
-      data = JSON.parse(data);
-      var feed = new RSS({
-        title: 'Producthunt RSS',
-        description: 'Producthunt RSS feed',
-      });
+    oa.get('https://api.producthunt.com/v1/posts/all', process.env.DEV_TOKEN, function(err, data) {
 
-      _.each(data.posts, function(post){
-        feed.item({
-          title: post.name + ' - ' + post.tagline,
-          description: '<img src="'+post.screenshot_url['850px']+'"><br><a href="'+post.discussion_url+'">Discussion link</a> - Votes: '+post.votes_count+' - Comments: '+post.comments_count,
-          url: post.redirect_url, // link to the item 
-          guid: post.id, // optional - defaults to url 
-          author: post.user.name + ' (@'+post.user.username+')', // optional - defaults to feed author property 
-          date: post.created_at, // any format that js Date can parse. 
+      if (err) {
+
+        res.send(err);
+
+      } else {
+
+        data = JSON.parse(data);
+        var feed = new RSS({
+          title: 'Producthunt RSS',
+          description: 'Producthunt RSS feed',
         });
-      });
 
-      var xml = feed.xml();
-      res.send(xml);
- 
-    }
+        _.each(data.posts, function(post){
+          feed.item({
+            title: post.name + ' - ' + post.tagline,
+            description: '<img src="'+post.screenshot_url['850px']+'"><br><a href="'+post.discussion_url+'">Discussion link</a> - Votes: '+post.votes_count+' - Comments: '+post.comments_count,
+            url: post.redirect_url, // link to the item 
+            guid: post.id, // optional - defaults to url 
+            author: post.user.name + ' (@'+post.user.username+')', // optional - defaults to feed author property 
+            date: post.created_at, // any format that js Date can parse. 
+          });
+        });
 
-  });
+        var xml = feed.xml();
+
+        cache.put('posts/all', xml, 300000)
+
+        res.send(xml);
+   
+      }
+
+    });
+
+  }
 
 });
 
